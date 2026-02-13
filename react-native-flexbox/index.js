399e63cb1e776581fc9c6152c04e97111190c3dd
Nathaniel Miguel David <miguelcastrodavid@gmail.com>
2026-02-13 17:00:11 +0800

    Done with Layout Direction Section

--- react-native-flexbox/index.js
+++ react-native-flexbox/index.js
@@ -1,9 +1,11 @@
 import { registerRootComponent } from 'expo';
 
-import FlexDirectionBasics from './components/FlexDirection';
+// import Flexs from './components/Flex';
+// import FlexDirectionBasics from './components/FlexDirection';
+import LayoutDirection from './components/LayoutDirection';
 
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(FlexDirectionBasics);
+registerRootComponent(LayoutDirection);
