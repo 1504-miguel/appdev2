f2d1a0cd4368c74ffc91a7fe7685f1b6417c6573
Nathaniel Miguel David <miguelcastrodavid@gmail.com>
2026-02-18 00:51:33 +0800

    Added Login screen title

--- react-native-layout/screens/Login.js
+++ react-native-layout/screens/Login.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 export default function Login() {
   return (
     <View style={styles.container}>
-      <Text>Login Screen</Text>
+      <Text style={styles.title}>Login</Text>
+
+      <TextInput placeholder="Email" style={styles.input}/>
+
     </View>
+    
   );
 }
 
@@ -13,6 +17,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
-  }
+    padding: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+
+  input: {
+  borderWidth: 1,
+  borderColor: '#ccc',
+  padding: 12,
+  borderRadius: 8,
+  marginTop: 20},
 });
